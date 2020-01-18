f0b2b5512ae82a8b6a17f55bdfac6bd26f53ca39
SidSan97 <sidnei1.8santiago@hotmail.com>
2020-01-18 19:15:57 -0300

    add nova função: 'onmouseenter'
    Altera a cor do botão de compra entre verde e azul, indicando que o botão de compra está no foco

--- Site comercial/js/comercial2.js
+++ Site comercial/js/comercial2.js
@@ -1,9 +1,9 @@
-document.getElementsByClassName('kj')[2].onmouseenter = function() {mouseEnter()};
-		document.getElementsByClassName('kj').onmouseleave = function() {mouseLeave()};
+document.getElementsByClassName('btn')[0].onmouseenter = function() {mouseEnter()};
+document.getElementsByClassName('btn')[0].onmouseleave = function() {mouseLeave()};
 
 		function mouseEnter() {
-        document.getElementsByClassName('kj')[2].style.background = 'red';
+        document.getElementsByClassName('btn')[0].style.background = 'rgb(26, 138, 212)';
 }     
 function mouseLeave() {
-  document.getElementsByClassName('kj').style.background = "red";
+  document.getElementsByClassName('btn')[0].style.background = "rgb(47, 158, 53)";
 }
